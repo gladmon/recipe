94dc6218bd63ba6d06b224408e7a1610dcd5c6f8
Robbie Gladmon <robbie@gladmon.com>
2013-03-04 00:20:00 -0500

    Trying to deploy to openstack still
    fixed the scripts to run during push and root of app

--- config/routes.rb
+++ config/routes.rb
@@ -56,4 +56,5 @@ Recipe::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
   resources :food_recipes, :except => :destroy
+  root :to => 'food_recipes#index'
 end
